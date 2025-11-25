38a33bb040f2b8c16d0fcb361ab651ad4b4f6e35
EduBonfim <134088977+EduBonfim@users.noreply.github.com>
2025-11-25 01:43:41 -0300

    Fix styleUrls syntax in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'Games4Life';
